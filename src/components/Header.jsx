90653ee6b9b507ef7b6319ed9fad41ab579f0082
Sushmitha Vanama <vanamasushmitha@gmail.com>
2023-12-19 17:02:56 +0530

    navbar bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ function Header() {
         </div>
       </nav> */}
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-black ">
         <div class="container-fluid">
           <a className="navbar-brand" href="/" style={{width:'65%'}}><img src={logoimg} style={{ width: '30%' }} alt="Logo"></img></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
